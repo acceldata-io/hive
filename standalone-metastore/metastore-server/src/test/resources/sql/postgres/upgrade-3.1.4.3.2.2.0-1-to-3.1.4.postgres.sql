1ec9bcf576281f11898f883303a17d014adaa63e
Manish Singh Mowall <manishsingh@acceldata.io>
2023-11-03 16:00:29 +0530

    ODP-826: Hive version shortname from 3.1.4.3.2.2.0-1 to 3.1.4

---
+++ standalone-metastore/metastore-server/src/test/resources/sql/postgres/upgrade-3.1.4.3.2.2.0-1-to-3.1.4.postgres.sql
@@ -0,0 +1,6 @@
+-- The file has some overlapping with upgrade-3.1.4.3.2.2.0-1-to-3.1.4.postgres.sql
+SELECT 'Upgrading MetaStore schema from 3.1.4.3.2.2.0-1 to 3.1.4';
+
+-- These lines need to be last. Insert any changes above.
+UPDATE "VERSION" SET "SCHEMA_VERSION"='3.1.4', "VERSION_COMMENT"='Hive release version 3.1.4' where "VER_ID"=1;
+SELECT 'Finished upgrading MetaStore schema from 3.1.4.3.2.2.0-1 to 3.1.4';
