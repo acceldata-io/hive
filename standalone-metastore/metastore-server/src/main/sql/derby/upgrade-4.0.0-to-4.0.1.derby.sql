6609ba324c9ead65cf16bb5023b2e08787f36985
Sourabh Dilraj <sourabh@acceldata.io>
2025-08-13 15:09:17 +0530

    Revert "ODP-4906 Add odp-hive's support for 4.0.1 schema upgrade"
    This reverts commit 61615ba004ca22cd82335524f1ce58b4b1cece22.

--- standalone-metastore/metastore-server/src/main/sql/derby/upgrade-4.0.0-to-4.0.1.derby.sql
+++
@@ -1,2 +0,0 @@
--- This needs to be the last thing done.  Insert any changes above this line.
-UPDATE "APP".VERSION SET SCHEMA_VERSION='4.0.1', VERSION_COMMENT='Hive release version 4.0.1' where VER_ID=1;
