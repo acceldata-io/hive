4e8ca2c7dd386bb02ac25103b52722320264028b
senthh <senthil.kumar@acceldata.io>
2026-09-02 21:39:39 +0530

    ODR-2601 - Bug - Make filesAdded optional for Spark insert DML events
    Allow legacy Spark/PySpark clients to fire_listener_event without filesAdded
    so NOTIFICATION_LOG insert events are recorded for replication-enabled DBs.
    Infer file paths from partition/table location when filesAdded is unset.

--- standalone-metastore/metastore-common/src/gen/thrift/gen-py/hive_metastore/ttypes.py
+++ standalone-metastore/metastore-common/src/gen/thrift/gen-py/hive_metastore/ttypes.py
@@ -18581,8 +18581,6 @@ class InsertEventRequestData(object):
         oprot.writeStructEnd()
 
     def validate(self):
-        if self.filesAdded is None:
-            raise TProtocolException(message='Required field filesAdded is unset!')
         return
 
     def __repr__(self):
